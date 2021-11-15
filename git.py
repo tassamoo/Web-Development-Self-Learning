06a4f29bbccfc2cb7d5caf9ef62cb05f5ecf5be3
tassamoo <tassamu@outlook.com>
2021-11-16 06:52:20 +0700

    test push

--- git.py
+++ git.py
@@ -4,4 +4,4 @@ import os
 os.system ("git add .")
 message = input("Message:")
 os.system ("git commit -m '" + message + "'")
-os.system ("sudo git push -u https://github.com/tassamoo/web_dev")
+os.system ("git push -u https://github.com/tassamoo/web_dev")
