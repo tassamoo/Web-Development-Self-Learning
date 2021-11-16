257612da2ab94251dbef75213bc730bafc47f040
Tassamu Akhsan Nugroho <42428833+tassamoo@users.noreply.github.com>
2021-11-16 14:55:04 +0700

    Delete git.py

--- git.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python3
-import os
-
-os.system ("git add .")
-message = input("Message:")
-os.system ("git commit -m '" + message + "'")
-os.system ("git push -u https://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@github.com/tassamoo/web_dev.git")
