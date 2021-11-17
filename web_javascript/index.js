6f657f7de8fca2b42fd76238eacccf2ba5195b92
tassamoo <tassamu@outlook.com>
2021-11-17 20:44:47 +0700

    intro to javascript from freecodecamp youtube channel

---
+++ web_javascript/index.js
@@ -0,0 +1,11 @@
+// document.getElementById('count-el').innerText = 5;
+
+let count = 0;
+
+console.log(count);
+
+let myAge = 22;
+
+console.log(myAge);
+
+// javascript move line by line from top to bottom
