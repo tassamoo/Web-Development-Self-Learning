a80a725f377d3f606e3ad7fd5f9a52788ab158d6
tassamoo <tassamu@outlook.com>
2021-11-19 17:59:17 +0700

    learning how to utilize funtions in javascript

--- web_javascript/index.js
+++ web_javascript/index.js
@@ -1,6 +1,6 @@
-// Define variables in JavaScript
+// //Define variables in JavaScript
 
-// // document.getElementById('count-el').innerText = 5;
+// document.getElementById('count-el').innerText = 5;
 
 // let count = 0;
 // console.log(count);
@@ -8,23 +8,25 @@
 // let myAge = 22;
 // console.log(myAge);
 
-// javascript move line by line from top to bottom
+// // javascript move line by line from top to bottom
 
-//Basic Math Operations
+
+// //Basic Math Operations
 // let firstBatch = 5
 // let secondBatch = 7
 // let count = firstBatch * secondBatch
 // console.log(count);
 
-// example of math operations
-// create two variables, myAge and humanDogRatio
-// multiply them together and store the result in myDogAge
-// log myDogAge to the console
+// // example of math operations
+// // create two variables, myAge and humanDogRatio
+// // multiply them together and store the result in myDogAge
+// // log myDogAge to the console
 
-    // let myAge = 22
-    // let humanDogRatio = 7
-    // let myDogAge = myAge * humanDogRatio
-    // console.log(myDogAge)
+//     let myAge = 22
+//     let humanDogRatio = 7
+//     let myDogAge = myAge * humanDogRatio
+//     console.log(myDogAge)
+
 
 // // reassigning and incrementing variables
 
@@ -47,15 +49,81 @@
 //     bonusPoints = bonusPoints + 45
 //     console.log(bonusPoints)
 
-//The OnClick event listener
 
-// initialize the count as 0
-// listen for clicks on the incremennt button
-// increment the count variable when the button is clicked
-// change the count-el in the index.html to reflect the new count
+// // The OnClick event listener
 
-function increment() {
-    let count = 0;
-    count = count + 1;
-    document.getElementById('count-el').innerText = count;
+// // initialize the count as 0
+// // listen for clicks on the incremennt button
+// // increment the count variable when the button is clicked
+// // change the count-el in the index.html to reflect the new count
+
+// function increment() {
+//     let count = 0;
+//     count = count + 1;
+//     document.getElementById('count-el').innerText = count;
+// }
+
+
+// // Using functions to write less code
+
+// function countdown() {
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
+
+// // setting up a race car game function
+// countdown()
+
+// Go
+// Players are running the Race
+// race is finished
+
+// get ready for a new race
+// countdown()
+
+
+// // writing my first function
+
+// // create a funtion (you decide the name) that logs out the number 42 to the console
+// // call/invoke the function
+
+// function myLogger(){
+//     console.log(42)
+// }
+
+// myLogger()
+
+// // challenge race game
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// // create a funtion that logs out the sum of all the lap times
+
+// function logLapTime(){
+//     let totalTime = lap1 + lap2 + lap3
+
+//     console.log(totalTime)
+// }
+
+// logLapTime()
+
+
+// write a function that increments
+
+let lapsCompleted = 0
+
+// create a function that increments the lapsCompleted variable with one
+// run it three times
+function incrementLaps(){
+    lapsCompleted = lapsCompleted + 1
 }
+
+incrementLaps()
+incrementLaps()
+incrementLaps()
+
+console.log(lapsCompleted)
