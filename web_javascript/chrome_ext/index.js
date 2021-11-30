e6cb5463d2b71cf3c50e0647dd4d3a21e041856a
tassamoo <tassamu@outlook.com>
2021-11-30 22:05:24 +0700

    continue learning how to create chrome extension

--- web_javascript/chrome_ext/index.js
+++ web_javascript/chrome_ext/index.js
@@ -1,3 +1,9 @@
-function saveLead() {
-    console.log("buttonClicked");
-}
+// function saveLead() {
+//     console.log("buttonClicked");
+// }
+
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    console.log("Buttton clicked")
+});
---
+++ web_javascript/chrome_ext/pracctice/practice.js
@@ -0,0 +1,5 @@
+let boxBtn = document.getElementById("box-btn")
+
+boxBtn.addEventListener("click", function() {
+    console.log("I want to open the box!")
+});
