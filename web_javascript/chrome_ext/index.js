4d25e40dfc0457e0d7447e3ba6ec83befe7a2781
tassamoo <tassamu@outlook.com>
2021-12-07 22:15:34 +0700

    cleanup the code and adding notes

--- web_javascript/chrome_ext/index.js
+++ web_javascript/chrome_ext/index.js
@@ -5,15 +5,6 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-  
-
-// //create function to trigger button click while pressing enter
-// inputEl.addEventListener("keyup", function(event) {
-//     if (event.key === "Enter") {
-//         inputBtn.enterBtn()
-//     }
-// })
-
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
@@ -40,18 +31,3 @@ function renderLeads(){
     ulEl.innerHTML = listItems
 }
 
-// // method 2: using append to add to the end of the list
-
-// function renderLeads(){
-//     let listItems = ""
-//     for (let i = 0; i < myLeads.length; i++) {
-//         const element = myLeads[i]
-//         //create element
-//         const li = document.createElement("li")
-//         //set text content
-//         li.textContent = element
-//         //append to ul
-//         ulEl.append(li)
-//     }
-//     ulEl.innerHTML = listItems
-// 
