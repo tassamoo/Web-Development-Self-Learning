178724baf8e9da599b02cd47d8a6738f9eb45850
tassamoo <tassamu@outlook.com>
2021-12-09 23:12:27 +0700

    refactoring code by passing arguments and edit styling

--- web_javascript/chrome_ext/index.js
+++ web_javascript/chrome_ext/index.js
@@ -4,34 +4,43 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-
-// Get the leads from localStorage
-// Store it in avariable, leadsFromLocalStorage
-// Log out the variable
-
-let leadsFromLocalStorage = JSON.parse (localStorage.getItem("leads"))
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse (localStorage.getItem("leads"))
 
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
-})
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
 
-
-function renderLeads(){
+function render(leads){
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        const element = myLeads[i]
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
         <li>
-            <a target='_blank' href='${element}'>
-                ${element}
+            <a target='_blank' href='${leads}'>
+                ${leads}
             </a>
         </li>
         `
     }
     ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+
+
 
