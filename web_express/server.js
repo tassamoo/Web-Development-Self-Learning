84d9a556a40f8cda406e239b42048b8685f29cc6
tassamoo <tassamu@outlook.com>
2021-12-27 23:44:01 +0700

    learn about routers

--- web_express/server.js
+++ web_express/server.js
@@ -3,14 +3,23 @@ const app = express()
 
 app.set('view engine', 'ejs') // you can also use pug besides ejs
 
+//basic routing is using app.**something**
 app.get('/', (req, res) => {
     
     console.log('Here')
     res.render("index", {text: "World"})
+    
+
+    // sending data to the client
     // res.download('./server.js')
     // res.status(500).json({
     //     message: "error"
     // })
 
 })
+
+const userRouter = require('./routes/users')
+
+app.use('/users', userRouter)
+
 app.listen(3000)
