b26f7817731e9b3f2a6ec7df1dbf3e7ac5d46a2d
tassamoo <tassamu@outlook.com>
2021-12-28 21:25:22 +0700

    learn how to use advanced routing

--- web_express/routes/users.js
+++ web_express/routes/users.js
@@ -9,4 +9,43 @@ router.get('/new', (req, res) => {
     res.send('User New Form')
 })
 
+router.post('/', (req, res) => {
+    res.send('User Created')
+})
+
+//clean code for router
+router
+    .route('/:id')
+    .get((req, res) => {
+        console.log(req.user)
+        res.send(`Get User With ID ${req.params.id}`)
+    })
+    .put((req, res) => {
+        res.send(`Update User With ID ${req.params.id}`)
+    })
+    .delete((req, res) => {
+        res.send(`Delete User With ID ${req.params.id}`)
+    })
+
+//if you use dynamic routing make sure the method is put below after you declare the first one
+
+// messy code for router same as above
+// router.get('/:id', (req, res) => {
+//     res.send(`Get User With ID ${req.params.id}`)
+// })
+
+// router.put('/:id', (req, res) => {
+//     res.send(`Update User With ID ${req.params.id}`)
+// })
+
+// router.delete('/:id', (req, res) => {
+//     res.send(`Delete User With ID ${req.params.id}`)
+// })
+
+const users = [{name: 'John'}, {name: 'Sara'}]
+router.param("id", (req, res, next, id) => {
+    req.user = users[id]
+    next()
+})
+
 module.exports = router
