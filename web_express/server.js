3b55532bfe3da6b6d7b9f13f005b95f79c618e97
tassamoo <tassamu@outlook.com>
2022-03-04 10:48:44 +0700

    basic routers

--- web_express/server.js
+++ web_express/server.js
@@ -1,15 +1,21 @@
 const express = require("express")
+const req = require("express/lib/request")
 const app = express()
 
-//Rendering HTML
-// crate views folder and index.html file
+//Routers
 
-app.set("view engine", "ejs") //setup view engine
+app.set("view engine", "ejs") 
 
 app.get('/', (req, res) => {
     console.log("Here")
     res.render("index",{text: "World"})
     
 })
+
+const userRouter = require("./routes/users")
+// const postRouter = require("./routes/posts")
+
+app.use("/users", userRouter)
+// app.use("/posts", postRouter)
 
 app.listen(3000)
